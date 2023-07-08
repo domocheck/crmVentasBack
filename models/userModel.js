66e1e78766eb578a958733ad22b69d82c25cffdd
Tomas luquez <luquez1431@gmail.com>
2023-07-08 17:46:45 -0300

    unique en el mail

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   name: {
     type: String,
     required: true,
@@ -12,11 +17,6 @@ const userSchema = new mongoose.Schema({
   role: {
     type: String,
     required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
   },
 });
 
