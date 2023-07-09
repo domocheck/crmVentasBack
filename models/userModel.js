e7a52493e6b6c4b75d3a4ad3d63752c5b244a4f7
Tomas luquez <luquez1431@gmail.com>
2023-07-09 18:32:28 -0300

    agregue relacion usuario con vendedor

--- models/userModel.js
+++ models/userModel.js
@@ -18,6 +18,9 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  vendedor: {
+    type: String
+  }
 });
 
 const User = mongoose.model("checkDeliveryUser", userSchema);
