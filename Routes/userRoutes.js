b3576bfea2e14351e7107c6bebdc917205a9b87a
Tomas luquez <luquez1431@gmail.com>
2023-07-14 19:13:54 -0300

    agregue ruta para obtener usuarios

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { getUser, createUser } from "../handlers/handlerUsers.js";
+import { getUser, createUser, getUsers } from "../handlers/handlerUsers.js";
 
 const router = Router();
 
-router.post("/login", getUser).post("/", createUser);
+router.post("/login", getUser).post("/", createUser).get("/", getUsers);
 
 export { router as userRoutes };
--- handlers/handlerUsers.js
+++ handlers/handlerUsers.js
@@ -47,3 +47,14 @@ export const getUser = async (req, res) => {
     res.status(404).json({ error: error });
   }
 };
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res
+      .status(200)
+      .json({ message: "todos los usuarios obtenidos", data: users });
+  } catch (error) {
+    res.status(404).json({ error: error });
+  }
+};
