ea9770bde26470b595e6703094811366e680d699
Tomas luquez <luquez1431@gmail.com>
2023-07-31 16:50:33 -0300

    agregue para cambiar de estado y borrar notificaciones

--- Routes/notificationsRoutes.js
+++ Routes/notificationsRoutes.js
@@ -2,10 +2,16 @@ import { Router } from "express";
 import {
   createNotifications,
   getNotifications,
+  deteleNotification,
+  chanceStatusNotification,
 } from "../handlers/handlerNotifications.js";
 
 const router = Router();
 
-router.post("/", createNotifications).get("/:idUser", getNotifications);
+router
+  .post("/", createNotifications)
+  .get("/:idUser", getNotifications)
+  .put("/", chanceStatusNotification)
+  .delete("/", deteleNotification);
 
 export { router as notiRoutes };
--- handlers/handlerNotifications.js
+++ handlers/handlerNotifications.js
@@ -32,3 +32,41 @@ export const getNotifications = async (req, res) => {
     res.status(404).json({ error: error });
   }
 };
+
+export const chanceStatusNotification = async (req, res) => {
+  try {
+    const { idNotification, idNotificationUser } = req.body;
+    const notification = await Notifications.findById(idNotification);
+
+    // Buscamos el índice del usuario en el array 'users' basado en el id del usuario
+    const userIndex = notification.users.findIndex(
+      (user) => user._id.toString() === idNotificationUser
+    );
+
+    // Si encontramos el usuario en el array 'users', actualizamos su estado
+    if (userIndex !== -1) {
+      notification.users[userIndex].statusNotification = "Leida";
+      await notification.save(); // ¡No olvides el await aquí para asegurarte de que la actualización se complete correctamente!
+      res
+        .status(200)
+        .json({ message: "Estado de notificación actualizado a 'Leida'." });
+    } else {
+      res
+        .status(404)
+        .json({ error: "Usuario no encontrado en la notificación." });
+    }
+  } catch (error) {
+    res.status(404).json({ error: error });
+  }
+};
+
+export const deteleNotification = async (req, res) => {
+  try {
+    const { idNotification } = req.params;
+    const notification = await Notifications.findById(idNotification);
+    await notification.remove();
+    res.status(200).json({ message: "notificacion eliminada" });
+  } catch (error) {
+    res.status(404).json({ error: error });
+  }
+};
