fe0951966df4840dd60afef0703e43aa34e602d6
Tomas luquez <luquez1431@gmail.com>
2023-09-08 19:23:24 -0300

    cambie el lugar de main

--- Services/sendReportAct.js
+++ Services/sendReportAct.js
@@ -1,8 +1,7 @@
 import Client from "../models/clientModel.js";
 import User from "../models/userModel.js";
-import nodemailer from "nodemailer";
 
-export const generateReportAct = async (req, res) => {
+export const generateReportAct = async () => {
   const fechaActual = new Date();
   const diaSemana = fechaActual.getDay();
   const lunesSemanaActual = new Date(
@@ -23,7 +22,7 @@ export const generateReportAct = async (req, res) => {
     lunesProximo: new Date(lunesSemanaActual),
   };
 
-  // Ahora, ajusta cada día de la semana según su posición
+  // Ajusta cada día de la semana según su posición
   semana.martes.setDate(semana.martes.getDate() + 1);
   semana.miercoles.setDate(semana.miercoles.getDate() + 2);
   semana.jueves.setDate(semana.jueves.getDate() + 3);
@@ -31,6 +30,8 @@ export const generateReportAct = async (req, res) => {
   semana.sabado.setDate(semana.sabado.getDate() + 5);
   semana.domingo.setDate(semana.domingo.getDate() + 6);
   semana.lunesProximo.setDate(semana.lunesProximo.getDate() + 7);
+
+  // Obtén los datos de la base de datos
   const clients = await Client.find();
   let actividadesCumplidas = [];
   let actividadesPendientes = [];
@@ -39,21 +40,19 @@ export const generateReportAct = async (req, res) => {
     clients.forEach((client) => {
       client.actividades.forEach((actividad) => {
         if (
-          (actividad.estadoAct =
-            "Pendiente" &&
-            new Date(actividad.proximoContacto) >= semana.lunes &&
-            new Date(actividad.proximoContacto) < semana.lunesProximo)
+          actividad.estadoAct === "Pendiente" &&
+          new Date(actividad.proximoContacto) >= semana.lunes &&
+          new Date(actividad.proximoContacto) < semana.lunesProximo
         ) {
-          actividadesPendientes.push(actividad); // Agregar al array en lugar de sobrescribir
+          actividadesPendientes.push(actividad);
         }
 
         if (
-          (actividad.estadoAct =
-            "Cumplida" &&
-            new Date(actividad.fechaCumplimiento) >= semana.lunes &&
-            new Date(actividad.fechaCumplimiento) < semana.lunesProximo)
+          actividad.estadoAct === "Cumplida" &&
+          new Date(actividad.fechaCumplimiento) >= semana.lunes &&
+          new Date(actividad.fechaCumplimiento) < semana.lunesProximo
         ) {
-          actividadesCumplidas.push(actividad); // Agregar al array en lugar de sobrescribir
+          actividadesCumplidas.push(actividad);
         }
       });
     });
@@ -63,19 +62,19 @@ export const generateReportAct = async (req, res) => {
   const actividadesPorUsuario = actividadesPorUsuarios(
     actividadesCumplidas,
     actividadesPendientes,
-    users
+    users,
+    semana
   );
 
-  res.status(200).json({
-    message: "todos los clientes obtenidos",
-    actividadesPorUsuario,
-  });
+  // Retorna los datos generados
+  return actividadesPorUsuario;
 };
 
 const actividadesPorUsuarios = (
   actividadesCumplidas,
   actividadesPendientes,
-  users
+  users,
+  semana
 ) => {
   const actividadesPorUsuario = [];
 
@@ -138,36 +137,6 @@ const actividadesPorUsuarios = (
       });
     }
   });
-
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: "luquez1431@gmail.com",
-      pass: "dtqlzwfwevhnjvfb",
-    },
-  });
-
-  transporter.verify().then(() => {
-    console.log("exito");
-  });
-
-  async function main() {
-    // send mail with defined transport object
-    const info = await transporter
-      .sendMail({
-        from: "luquez1431@gmail.com", // sender address
-        to: "luquez1431@gmail.com", // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>", // html body
-      })
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
-  }
-
-  main();
 
   return actividadesPorUsuario;
 };
--- index.js
+++ index.js
@@ -8,37 +8,74 @@ import { userRoutes } from "./Routes/userRoutes.js";
 import { notiRoutes } from "./Routes/notificationsRoutes.js";
 import { reportsRoutes } from "./Routes/reportesRoutes.js";
 import cron from "node-cron";
+import nodemailer from "nodemailer"; // Importa nodemailer
 import { generateReportAct } from "./Services/sendReportAct.js";
 
-// iniciamos el servidor
+// Inicializa el servidor
 const app = express();
 
-// corremos las variables de entorno
+// Configura las variables de entorno
 dotenv.config();
 
-//middlewares
+// Middleware
 app.use(express.json());
 app.use(cors());
 
-//conectar a la db
+// Conecta a la base de datos
 connectDB();
 
-//creamos los endpoints
+// Rutas de la API
 app.use("/api/clientes", clientesRoutes);
 app.use("/api/actividades", actividadesRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/notifications", notiRoutes);
 app.use("/api/reports", reportsRoutes);
 app.use("*", (req, res) => {
-  res.send("Para trabajar con la api, debes usar el endpoint /api/clientes");
+  res.send("Para trabajar con la API, debes usar el endpoint /api/clientes");
 });
 
-cron.schedule("10 19 * * 5", () => {
-  generateReportAct(); // Llama a tu función de envío de informe aquí
+// Programa la tarea cron para las 19:10 (10 minutos después de las 19:00) los viernes
+cron.schedule("30 19 * * 5", () => {
+  console.log("Ejecutando tarea cron a las 19:10");
+  main(); // Llama a la función main para generar y enviar el informe
 });
 
-// levantamos el server
+// Levanta el servidor
 const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => {
-  console.log("listening on port " + PORT);
+  console.log("Escuchando en el puerto " + PORT);
 });
+
+// Función para enviar correos electrónicos
+async function main() {
+  try {
+    const reportData = await generateReportAct(); // Llama a generateReportAct y obtén los datos
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "luquez1431@gmail.com",
+        pass: "dtqlzwfwevhnjvfb",
+      },
+    });
+
+    transporter.verify().then(() => {
+      console.log("Configuración de correo electrónico exitosa");
+    });
+
+    const info = await transporter.sendMail({
+      from: "luquez1431@gmail.com", // Reemplaza con tu dirección de correo
+      to: "luquez1431@gmail.com", // Reemplaza con el destinatario
+      subject: "Informe Semanal de Ventas", // Asunto del correo
+      text: "Adjunto se encuentra el informe semanal de ventas.", // Cuerpo del correo
+      html: `<p>Adjunto se encuentra el informe semanal de ventas:</p>
+             <pre>${JSON.stringify(reportData, null, 2)}</pre>`, // Agrega los datos del informe en el cuerpo del correo como texto o HTML
+    });
+
+    console.log("Correo electrónico enviado:", info.response);
+  } catch (error) {
+    console.error("Error al enviar el correo electrónico:", error);
+  }
+}
